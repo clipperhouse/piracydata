e560ba4a55c1c5dc5605dcfca79d50b49222dfd3
Matt Sherman <mwsherman@gmail.com>
2013-09-30 23:10:30 -0400

    correct description in database

--- database/database.go
+++ database/database.go
@@ -7,8 +7,8 @@ import (
 	"os"
 )
 
-// for local connection, set up postgres with user eyepatch, create database eyepatch, then:
-// export DATABASE_URL="dbname=eyepatch host=localhost port=5432 sslmode=disable user=eyepatch"
+// for local connection, set up postgres with user piracydata, create database piracydata, then:
+// export DATABASE_URL="dbname=piracydata host=localhost port=5432 sslmode=disable user=piracydata"
 // Heroku uses the DATABASE_URL enivronment variable, though theirs is an actual URL
 // can't use URL-style locally with postgres.app, as there is no way to disable SSL
 
