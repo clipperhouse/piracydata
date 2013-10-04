8719c64c23ee5a10e4eae2a9fb4212ac0cf96c92
Matt Sherman <mwsherman@gmail.com>
2013-10-03 23:11:46 -0400

    remove session and flash filters, they're adding cookies

--- app/init.go
+++ app/init.go
@@ -14,12 +14,12 @@ func init() {
 		revel.RouterFilter,            // Use the routing table to select the right Action
 		revel.FilterConfiguringFilter, // A hook for adding or removing per-Action filters.
 		revel.ParamsFilter,            // Parse parameters into Controller.Params.
-		revel.SessionFilter,           // Restore and write the session cookie.
-		revel.FlashFilter,             // Restore and write the flash cookie.
-		revel.ValidationFilter,        // Restore kept validation errors and save new ones from cookie.
-		revel.I18nFilter,              // Resolve the requested language
-		revel.InterceptorFilter,       // Run interceptors around the action.
-		revel.ActionInvoker,           // Invoke the action.
+		// revel.SessionFilter,           // Restore and write the session cookie.
+		// revel.FlashFilter,             // Restore and write the flash cookie.
+		revel.ValidationFilter,  // Restore kept validation errors and save new ones from cookie.
+		revel.I18nFilter,        // Resolve the requested language
+		revel.InterceptorFilter, // Run interceptors around the action.
+		revel.ActionInvoker,     // Invoke the action.
 	}
 
 	revel.OnAppStart(func() {
