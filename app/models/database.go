36ab3f3057bc5ee5b0445642d599c84f756923fd
Matt Sherman <mwsherman@gmail.com>
2013-10-05 15:51:55 -0400

    more ssl mode detection

--- app/models/database.go
+++ app/models/database.go
@@ -6,14 +6,27 @@ import (
 	_ "github.com/lib/pq"
 	"log"
 	"os"
+	"strings"
 )
 
 func openDb() *sql.DB {
 	connection := os.Getenv("DATABASE_URL")
 	sslmode := os.Getenv("PGSSLMODE") // based on https://github.com/lib/pq/commit/8875df52e9844f4c3fce993c8598bbd1c95c8a0f
 	log.Println("sslmode: " + sslmode)
+
 	if sslmode == "" {
-		os.Setenv("PGSSLMODE", "disable")
+		is_heroku := false
+		for _, env := range os.Environ() {
+			if strings.HasPrefix(env, "HEROKU_POSTGRESQL") {
+				is_heroku = true
+				break
+			}
+		}
+		if is_heroku {
+			os.Setenv("PGSSLMODE", "required")
+		} else {
+			os.Setenv("PGSSLMODE", "disable")
+		}
 	}
 	log.Println(connection)
 
